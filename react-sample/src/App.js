fc156ab20306cac23c74be76b150a85d38054475
Jeongin Park <jjeonginpark@gmail.com>
2019-11-21 11:42:05 +0900

    React 수정.

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+//리액트야 Subject라는 이름의 태그를 만들거야. 사용자 정의 태그
+class Subject extends Component {
+  //이 태그의 내용은 아래와 같아.
+  render() {
+    return(
+      <header>
+        <h1>WEB</h1>
+        World Wide Web
       </header>
-    </div>
-  );
+    )
+  }
+}
+
+class App extends Compoment {
+  render () {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Subject></Subject>
+        <nav>
+          <ol>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+          </ol>
+        </nav>
+        <article>
+            <h2>Welcom</h2>
+            Hello React
+        </article>
+      </div>
+    );
+  }
 }
 
 export default App;
