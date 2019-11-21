8035345bf9410d23c65e0fbc5a088dff5a8d5b6a
Jeongin Park <jjeonginpark@gmail.com>
2019-11-21 13:41:47 +0900

    사용자정의태그 만듦

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,26 +1,23 @@
-import React, {Component} from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import React from 'react';
 
-//리액트야 Subject라는 이름의 태그를 만들거야. 사용자 정의 태그
-class Subject extends Component {
-  //이 태그의 내용은 아래와 같아.
-  render() {
-    return(
+//사용자 적용 태그를 만들 수 있음.
+class Subject extends React.Component {
+  render(){
+    return (
       <header>
         <h1>WEB</h1>
         World Wide Web
       </header>
-    )
+    );
   }
 }
-
-class App extends Compoment {
-  render () {
+class App extends React.Component {
+  render() {
     return (
       <div className="App">
+
         <Subject></Subject>
-        <Subject></Subject>
+
         <nav>
           <ol>
             <li><a href="1.html">HTML</a></li>
@@ -29,12 +26,37 @@ class App extends Compoment {
           </ol>
         </nav>
         <article>
-            <h2>Welcom</h2>
-            Hello React
+          <h2>Welcom</h2>
+          Hello React
         </article>
-      </div>
+        </div>
     );
   }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//       <header>
+//         <h1>WEB</h1>
+//         World Wide Web
+//       </header>
+//       <Subject></Subject>
+//       <nav>
+//         <ol>
+//           <li><a href="1.html">HTML</a></li>
+//           <li><a href="2.html">CSS</a></li>
+//           <li><a href="3.html">JavaScript</a></li>
+//         </ol>
+//        </nav>
+//       <article>
+//           <h2>Welcom</h2>
+//           Hello React
+//       </article>
+//     </div>
+//   );
+// }
 
 export default App;
