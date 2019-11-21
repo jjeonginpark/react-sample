f44da4cb1c7898f7567349be6eba6bc9d2447e6e
Jeongin Park <jjeonginpark@gmail.com>
2019-11-21 14:16:29 +0900

    리스트 뿌리기

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -5,58 +5,67 @@ class Subject extends React.Component {
   render(){
     return (
       <header>
-        <h1>WEB</h1>
-        World Wide Web
+      <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
 }
-class App extends React.Component {
-  render() {
+
+class Toc extends React.Component {
+  render(){
+    var list = [];
+    // var i = 0;
+    // while(i<this.props.data.length){
+    //   var item = this.props.data[i];
+    //   list.push(<li><a href={item.id+'.html'}>{item.title}</a></li>);
+    //   i = i + 1;
+    // }
+
+    this.props.data.map(function (item) {
+      list.push(<li key={item.id}><a href={item.id+'.html'}>{item.title}</a></li>);
+    });
+
     return (
-      <div className="App">
-
-        <Subject></Subject>
-
-        <nav>
-          <ol>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-          </ol>
-        </nav>
-        <article>
-          <h2>Welcom</h2>
-          Hello React
-        </article>
-        </div>
+      <nav>
+        <ol>
+          {list}
+        </ol>
+      </nav>
     );
   }
 }
 
+class Content extends React.Component {
+  render(){
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
 
-// function App() {
-//   return (
-//     <div className="App">
+class App extends React.Component {
+  state = {
+    contents : [
+      {id:1, title:'HTML', desc:'HTML is for imformation'},
+      {id:2, title:'CSS', desc:'CSS is for design'},
+      {id:3, title:'JavaScript', desc:'JavaScript is for interaction'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
 
-//       <header>
-//         <h1>WEB</h1>
-//         World Wide Web
-//       </header>
-//       <Subject></Subject>
-//       <nav>
-//         <ol>
-//           <li><a href="1.html">HTML</a></li>
-//           <li><a href="2.html">CSS</a></li>
-//           <li><a href="3.html">JavaScript</a></li>
-//         </ol>
-//        </nav>
-//       <article>
-//           <h2>Welcom</h2>
-//           Hello React
-//       </article>
-//     </div>
-//   );
-// }
+        <Subject title="Web" sub="World Wide Web"></Subject>
+        <Toc data={this.state.contents}></Toc>
+        <Content title="Welcom" desc="Hello React"></Content>
+        
+      </div>
+    );
+  }
+}
 
 export default App;
